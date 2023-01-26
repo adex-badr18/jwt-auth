e483d17fcb079d1c7d8159e6c60dce3906492799
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-01-26 09:40:54 +0100

    Add request that refreshes user token after expiration

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 function jwtTokens({ user_id, username, email }) {
     const user = { user_id, username, email };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '3m' });
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '40s' });
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '5m' });
     return { accessToken, refreshToken };
 }
